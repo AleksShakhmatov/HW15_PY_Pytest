af6f77c9a480aafb0d3253645563100fa3e0ea0c
AleksSh <Coluchy@yandex.ru>
2024-10-22 00:32:55 +0600

    code for HW was added

---
+++ tests/conftest.py
@@ -0,0 +1,44 @@
+import pytest
+from selene import browser
+
+
+@pytest.fixture(scope='function', params=[(1440, 1080), (1280, 720), (1920, 1080)],
+                ids=['desktop_1440_1080', 'desktop_1280_720', 'desktop_1920_1080'])
+def browser_desktop(request):
+    browser.config.base_url = 'https://github.com'
+    width, height = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture(scope='function', params=[(360, 740), (390, 844), (412, 915)],
+                ids=['mobile_360_740', 'mobile_390_844', 'mobile_412_915'])
+def browser_mobile(request):
+    browser.config.base_url = 'https://github.com'
+    width, height = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
+
+    yield
+
+    browser.quit()
+
+
+@pytest.fixture(scope='function', params=[(1440, 1080), (1920, 1080), (360, 740), (412, 915)],
+                ids=['desktop_1440_1080', 'desktop_1920_1080', 'mobile_360_740', 'mobile_412_915'])
+def browser_config(request):
+    browser.config.base_url = 'https://github.com'
+    width, height = request.param
+    browser.config.window_width = width
+    browser.config.window_height = height
+
+    if width >= 800:
+        yield 'desktop'
+    else:
+        yield 'mobile'
+
+    browser.quit()
---
+++ tests/test_parametrize.py
@@ -0,0 +1,22 @@
+from selene import browser, be
+import pytest
+
+desktop = pytest.mark.parametrize('browser_config', [(1440, 1080), (1920, 1080)], indirect=True,
+                                  ids=['desktop_1440_1080', 'desktop_1920_1080'])
+mobile = pytest.mark.parametrize('browser_config', [(360, 740), (412, 915)], indirect=True,
+                                 ids=['mobile_360_740', 'mobile_412_915'])
+
+
+@desktop
+def test_github_desktop(browser_config):
+    browser.open('/')
+    browser.element('.HeaderMenu-link--sign-up').click()
+    browser.element('#email').should(be.visible)
+
+
+@mobile
+def test_github_mobile(browser_config):
+    browser.open('/')
+    browser.element('.Button--link').click()
+    browser.element('.HeaderMenu-link--sign-up').click()
+    browser.element('#email').should(be.visible)
